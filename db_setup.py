d0af860b39829fcd09f7b1c8d84b0f0eafd6f783
lucas-r-cooper <lucas.r.cooper@gmail.com>
2017-06-07 23:15:36 -0400

    Add db setup script

---
+++ db_setup.py
@@ -0,0 +1,21 @@
+# This file will create the database file necessary for the bot
+# Please run this only once
+
+import sqlite3
+import irc_cfg
+db_conn = sqlite3.connect('bot.db')
+conn_cursor = db_conn.cursor()
+owner = irc_cfg.CHAN[1:]
+t = (owner,)
+print t
+
+#Create tables
+conn_cursor.execute("CREATE TABLE chat_points (username varchar(255) PRIMARY KEY, points int);")
+conn_cursor.execute("CREATE TABLE bonus_chat_points (username varchar(255) PRIMARY KEY, points int);")
+conn_cursor.execute("CREATE TABLE chatted_today (username varchar(255) PRIMARY KEY);")
+conn_cursor.execute("CREATE TABLE mods (username varchar(255) PRIMARY KEY);")
+conn_cursor.execute("CREATE TABLE admins (username varchar(255) PRIMARY KEY);")
+
+#Add first admin as streamer, irc channel name
+conn_cursor.execute("INSERT INTO admins values(?);", t)
+db_conn.commit()
