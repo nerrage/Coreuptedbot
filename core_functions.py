ba5e045f12340ee047597e0b961ec2f25fb87ab0
lucas-r-cooper <lucas.r.cooper@gmail.com>
2017-06-08 00:46:35 -0400

    Add get points function

--- core_functions.py
+++ core_functions.py
@@ -1,6 +1,10 @@
 import requests
 import irc_cfg
 import json
+import sqlite3
+
+db_conn = sqlite3.connect('bot.db')
+conn_cursor = db_conn.cursor()
 
 owner = irc_cfg.CHAN[1:]
 
@@ -15,3 +19,24 @@ def getchatlist():
     chat_list.extend(bad_hombres["chatters"]["global_mods"])
     chat_list.extend(bad_hombres["chatters"]["viewers"])
     return chat_list
+
+def user_exists(username, table):
+    #Return 1 if user is on a table
+    #Return 0 if user is not on table
+    t = (username,)
+    conn_cursor.execute("SELECT COUNT(*) FROM "+table+" WHERE username = ?", t)
+    result = conn_cursor.fetchone()
+    return result[0]
+
+
+def givepoints(username, gift_points):
+    #Give points to a user, create record if not 
+    #TODO add int validation
+    t = (gift_points,username)
+    if user_exists(username, 'chat_points'):
+        conn_cursor.execute("UPDATE chat_points SET points = points + ? WHERE username = ?", t)
+        db_conn.commit()
+    else:
+        conn_cursor.execute("INSERT INTO chat_points(points,username) values(?,?)", t)
+        db_conn.commit()
+    return
